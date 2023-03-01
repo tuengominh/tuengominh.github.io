05644f1aa4b06aa74c1763adb6ab859eced6f8da
Tue M. Ngo <tue.ngominh@gmail.com>
2023-03-01 12:39:50 +0100

    add serach bar

---
+++ assets/js/search.js
@@ -0,0 +1,11 @@
+function search_project() { 
+    let input = document.getElementById('search-input').value 
+    input = input.toLowerCase(); 
+
+    let x = document.getElementsByClassName('search_results'); 
+    for (i = 0; i < x.length; i++) {  
+        if (!x[i].innerHTML.toLowerCase().includes(input)) { 
+            x[i].style.display="none"; 
+        } 
+    } 
+} 
