3bf530a3455296848a23bb4042ef4ca13061271a
Tue M. Ngo <tue.ngominh@gmail.com>
2023-03-04 15:06:13 +0100

    test search bar

--- assets/js/search.js
+++ assets/js/search.js
@@ -4,8 +4,14 @@ function search_project(){
 
     let x = document.getElementsByClassName('search_result'); 
     for (i = 0; i < x.length; i++){  
-        if (!x[i].innerHTML.toLowerCase().includes(input)){ 
-            x[i].style.display = "none"; 
+        if (input === ""){ 
+            x[i].style.display = ""; 
+        } else {
+            if (x[i].innerHTML.toLowerCase().includes(input)){ 
+                x[i].style.display = ""; 
+            } else {
+                x[i].style.display = "none";
+            }
         }
     } 
 } 
