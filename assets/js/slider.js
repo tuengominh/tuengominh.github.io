0dae4f5db58f461578f74a05cd154b99c8d2064b
Tue M. Ngo <tue.ngominh@gmail.com>
2023-03-04 18:31:37 +0100

    test slider

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -1,43 +1,21 @@
-const state = {};
-const carouselList = document.querySelector('.carousel_list');
-const carouselItems = document.querySelectorAll('.carousel_item');
-const elems = Array.from(carouselItems);
+$('#carousel').carousel({
+  interval: 10000
+})
 
-carouselList.addEventListener('click', function (event) {
-  var newActive = event.target;
-  var isItem = newActive.closest('.carousel_item');
-
-  if (!isItem || newActive.classList.contains('carousel_item_active')) {
-    return;
-  };
-  
-  update(newActive);
+$('.carousel .carousel-item').each(function(){
+    var minPerSlide = 3;
+    var next = $(this).next();
+    if (!next.length) {
+    next = $(this).siblings(':first');
+    }
+    next.children(':first-child').clone().appendTo($(this));
+    
+    for (var i=0;i<minPerSlide;i++) {
+        next=next.next();
+        if (!next.length) {
+        	next = $(this).siblings(':first');
+      	}
+        
+        next.children(':first-child').clone().appendTo($(this));
+      }
 });
-
-const update = function(newActive) {
-  const newActivePos = newActive.dataset.pos;
-
-  const current = elems.find((elem) => elem.dataset.pos == 0);
-  const prev = elems.find((elem) => elem.dataset.pos == -1);
-  const next = elems.find((elem) => elem.dataset.pos == 1);
-  const first = elems.find((elem) => elem.dataset.pos == -2);
-  const last = elems.find((elem) => elem.dataset.pos == 2);
-  
-  current.classList.remove('carousel_item_active');
-  
-  [current, prev, next, first, last].forEach(item => {
-    var itemPos = item.dataset.pos;
-
-    item.dataset.pos = getPos(itemPos, newActivePos)
-  });
-};
-
-const getPos = function (current, active) {
-  const diff = current - active;
-
-  if (Math.abs(current - active) > 2) {
-    return -current
-  }
-
-  return diff;
-}
